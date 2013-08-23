a53905a310afc1b729181f876924500b26e44df4
Colin Decker <cgdecker@google.com>
2013-08-23 09:00:42 -0700

    Remove reference to AttributeService in attribute package javadoc.

--- src/main/java/com/google/common/io/jimfs/attribute/package-info.java
+++ src/main/java/com/google/common/io/jimfs/attribute/package-info.java
@@ -24,9 +24,6 @@
  * Each attribute provider may also declare that it "inherits" one or more other attribute groups,
  * allowing it to provide the attributes that group provides. For example, "posix" inherits
  * "basic", allowing "posix:isDirectory" to return the basic attribute "isDirectory".
- *
- * <p>One or more attribute providers are combined in an {@link AttributeService}, which acts as
- * a central service for all attribute handling for a file system instance.
  */
 package com.google.common.io.jimfs.attribute;
 
