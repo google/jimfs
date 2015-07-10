22669a94f8bfc6a3bdbf1fcd9b1a21a46fa58fe3
Colin Decker <cgdecker@google.com>
2015-07-10 15:45:33 -0400

    Add some things copied from Guava

---
+++ _util/util.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+set -e -u
+
+# Exits if there are uncommitted changes in the git repo.
+function ensure_no_uncommitted_changes {
+  if ! git diff --quiet ; then
+    echo "Uncommitted changes found. Aborting." >&2
+    exit 1
+  fi
+}
+
+# Returns the current git branch, if any; the HEAD commit's SHA1 otherwise.
+function current_git_ref {
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  if [ $branch == "HEAD" ]; then
+    echo $(git rev-parse HEAD)
+  else
+    echo $branch
+  fi
+}
+
+# Returns the version of com.google.jimfs:jimfs at the current revision, pulled from Maven.
+function maven_version {
+  mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate \
+      -Dexpression=project.version \
+      -pl jimfs \
+      | grep -Ev '(^\[|Download\w+:)'
+}
+
+# Checks that a given tag exists in the repo.
+function check_tag_exists {
+  tag=$1
+  if ! git show-ref -q --verify refs/tags/$tag; then
+    echo "Tag $tag does not exist" >&2
+    exit 1
+  fi
+}
+
+# Takes an input arg representing a version (as parsed in parse_version) and
+# returns the git ref (tag or branch) that should be checked out to get that version.
+function git_ref {
+  release=$1
+  if [[ $release == "snapshot" ]]; then
+    echo "master"
+  else
+    tag="v$release"
+    check_tag_exists $tag
+    echo $tag
+  fi
+}
+
+# Checks out the branch/ref with the given identifier.
+# If the ref is the master branch, pulls to update it.
+function git_checkout_ref {
+  ref=$1
+  echo -n "Checking out '$ref'..."
+  git checkout -q $ref
+  echo " Done."
+
+  # If we're on master, pull to get the latest
+  if [ $ref == "master" ]; then
+    echo -n "Pulling to get latest changes..."
+    git pull -q --ff-only
+    echo " Done."
+  fi
+}
+
+platform=$(uname)
+if [[ $platform == "Linux" ]]; then
+  # GNU utils
+  extended="-r"
+  versionsort="--version-sort"
+else
+  # BSD utils
+  extended="-E"
+  versionsort="-g"
+fi
+
+# Sorts all numeric releases from the _releases/ directory by version, from
+# greatest version to least. This works as you'd expect, for example:
+#
+#   18.0.2 > 18.0.1 > 18.0 > 18.0-rc2 > 18.0-rc1 > 17.1 > 17.0.1 > 17.0
+#
+# This function expects to be run with the working directory at the root of
+# the git tree.
+function sort_releases {
+  # This is all sorts of hacky and I'm sure there's a better way, but it
+  # seems to work as long as we're just dealing with versions like 1.2,
+  # 1.2.3, 1.2-rc1 and 1.2.3-rc1.
+  ls _releases | \
+      grep -E ^[0-9]+\.[0-9]+ | \
+      sort -u | \
+      sed $extended -e 's/^([0-9]+\.[0-9]+)$/\1.01/g' -e 's/-rc/!/g' | \
+      sort -r $versionsort | \
+      sed $extended -e 's/!/-rc/g' -e 's/\.01//g'
+}
+
+# Gets the major version part of a version number.
+function major_version {
+  majorversion=$(echo "$1" | cut -d . -f 1)
+  if [[ ! $majorversion =~ ^[0-9]+$ ]]; then
+    echo "Invalid version number: $1" >&2
+    exit 1
+  fi
+  echo $majorversion
+}
+
+# Prints the highest non-rc release from the sorted list of releases
+# produced by sort_releases. If a release argument is provided, print
+# the highest non-rc release that has a major version that is lower than
+# the given release. For example, given "16.0.1", return "15.0".
+function latest_release {
+  if [[ $# -eq 1 ]]; then
+    ceiling=$(major_version "$1")
+  else
+    ceiling=""
+  fi
+
+  releases=$(sort_releases)
+  for release in $releases; do
+    if [[ ! -z "$ceiling" ]]; then
+      releasemajor=$(major_version "$release")
+      if (( ceiling <= releasemajor )); then
+        continue
+      fi
+    fi
+    if [[ ! $release =~ ^.+-rc[0-9]+$ ]]; then
+      echo $release
+      break
+    fi
+  done
+}
---
+++ updaterelease.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+
+#***************************************************************************
+#
+# Main script for updating release API docs and diffs. Can be used to
+# either update a specific release version or the current snapshot release.
+#
+# Usage examples:
+#
+#   ./updaterelease.sh snapshot
+#   ./updaterelease.sh 18.0
+#   ./updaterelease.sh 18.0-rc1
+#
+# All of these update the Javadoc located at _releases/<release>/api/docs
+# and the JDiff located at _releases/<release>/api/diffs, creating those
+# directories if this is a new release version. If <release> is 'snapshot',
+# Javadoc and JDiff is derived from the 'master' branch. Otherwise, it is
+# derived from the git tag 'v<release>'. In both cases, the actual version
+# number is determined by checking out the git branch or tag and getting
+# the version number from the pom.xml file via Maven (for non-snapshot
+# releases, though, it should always be the same as the <release>
+# argument).
+#
+# For all releases, JDiff compares the release to the previous non-rc
+# release. For example, snapshot is compared against 18.0 (even if there's
+# a 19.0-rc1 out) and 18.0 is compared against 17.0 (or 17.1 or 17.0.1 if
+# there were one of those).
+#
+#***************************************************************************
+
+set -e -u
+
+# Ensure working dir is the root of the git repo and load util functions.
+cd $(dirname $0)
+source _util/util.sh
+
+ensure_no_uncommitted_changes
+
+# Ensure valid args from user and get the basic variables we need.
+if [[ ! $# -eq 1 ]]; then
+  echo "Usage: $0 <release>" >&2
+  exit 1
+fi
+release=$1
+releaseref=$(git_ref $release)
+initialref=$(current_git_ref)
+
+# Create temp directories and files.
+tempdir=$(mktemp -d -t release-$release-temp.XXX)
+logfile=$(mktemp -t release-$release-temp-log.XXX)
+
+# Ensure temp files are cleaned up and we're back on the original branch on exit.
+function cleanup {
+  exitcode=$? 
+  if [[ "$exitcode" == "0" ]]; then
+    rm $logfile
+  else
+    # Put a newline in case we're in the middle of a "Do something... Done." line
+    echo ""
+    echo "Update failed: see log at '$logfile' for more details." >&2
+    # If we failed while not on the original branch/ref, switch back to it.
+    currentref=$(current_git_ref)
+    if [[ "$currentref" != "$initialref" ]]; then
+      git checkout -q $initialref
+    fi
+  fi
+  rm -fr $tempdir
+  exit $exitcode
+}
+trap cleanup INT TERM EXIT
+
+# Switch to the git ref for the release to do things with the actual Guava repo.
+git_checkout_ref $releaseref
+
+# Get the current version from Maven.
+mavenversion=$(maven_version)
+
+echo "Updating Javadoc and JDiff for Jimfs $mavenversion"
+
+# Copy source files to a temp dir.
+cp -r jimfs/src/main/java $tempdir/src
+
+# Compile and generate Javadoc, putting class files in $tempdir/classes and docs in $tempdir/docs.
+
+echo -n "Compiling and generating Javadoc..."
+mvn \
+    clean \
+    compile \
+    javadoc:javadoc \
+    dependency:build-classpath \
+    -Dmdep.outputFile=$tempdir/classpath \
+    -pl jimfs >> $logfile 2>&1
+echo " Done."
+
+mv jimfs/target/classes $tempdir/classes
+mv jimfs/target/site/apidocs $tempdir/docs
+
+# Create classpath string for JDiff to use.
+classpath=$tempdir/classes:$(cat $tempdir/classpath)
+
+# Cleanup target dir.
+rm -fr jimfs/target
+
+# Switch back to gh-pages.
+git_checkout_ref $initialref
+
+# Generate JDiff XML file for the release.
+echo -n "Generating JDiff XML..."
+jdiffpath="_util/lib/jdiff.jar:_util/lib/xerces-for-jdiff.jar"
+javadoc \
+  -sourcepath $tempdir/src \
+  -classpath $classpath \
+  -subpackages com.google.common \
+  -encoding UTF-8 \
+  -doclet jdiff.JDiff \
+  -docletpath $jdiffpath \
+  -apiname "Guava $mavenversion" \
+  -apidir $tempdir \
+  -exclude com.google.common.base.internal \
+  -protected >> $logfile 2>&1
+echo " Done."
+
+# Get the previous release version to diff against.
+echo -n "Determining previous release version..."
+prevrelease=$(latest_release $mavenversion)
+
+mkdir $tempdir/diffs
+
+if [ -z "$prevrelease" ]; then
+  echo " no previous release found"
+  echo "Not generating JDiff comparison"
+else
+  echo " $prevrelease"
+
+  cp _releases/$prevrelease/api/diffs/$prevrelease.xml $tempdir/Guava_$prevrelease.xml
+
+  # Generate Jdiff report, putting it in $tempdir/diffs
+
+  # These are the base paths to Javadoc that will be used in the generated changes html files.
+  # Use paths relative to the directory where those files will go (_releases/$release/api/diffs/changes).
+  # _releases/$release/api/docs/
+  releasejavadocpath="../../docs/"
+  # _releases/$prevrelease/api/docs/
+  prevjavadocpath="../../../../$prevrelease/api/docs/"
+
+  echo -n "Generating JDiff report between Guava $prevrelease and $mavenversion..."
+  javadoc \
+    -subpackages com \
+    -doclet jdiff.JDiff \
+    -docletpath $jdiffpath \
+    -oldapi "Guava $prevrelease" \
+    -oldapidir $tempdir \
+    -newapi "Guava $mavenversion" \
+    -newapidir $tempdir \
+    -javadocold $prevjavadocpath \
+    -javadocnew $releasejavadocpath \
+    -d $tempdir/diffs >> $logfile 2>&1
+  echo " Done."
+
+  # Make changes to the JDiff output
+  # Remove the useless user comments xml file
+  rm $tempdir/diffs/user_comments_for_Guava_*
+
+  # Change changes.html to index.html, making the url for a diff just _releases/<release>/api/diffs/
+  mv $tempdir/diffs/changes.html $tempdir/diffs/index.html
+  # Change references to ../changes.html in the changes/ subdirectory  to reference the new URL (just ..)
+  find $tempdir/diffs/changes -name *.html -exec sed -i'.bak' -e 's#\.\./changes.html#..#g' {} ";"
+  # Just create backup files and then delete them... doesn't seem to be any good way to avoid this if
+  # you want this to work with either GNU or BSD sed.
+  rm $tempdir/diffs/changes/*.bak
+fi
+
+# Put the generated JDiff XML file in the correct place in the diffs dir.
+mv $tempdir/Guava_$mavenversion.xml $tempdir/diffs/$release.xml
+
+# Move generated output to the appropriate final directories.
+docsdir=_releases/$release/api/docs
+mkdir -p $docsdir && rm -fr $docsdir
+diffsdir=_releases/$release/api/diffs
+mkdir -p $diffsdir && rm -fr $diffsdir
+
+echo -n "Moving generated Javadoc to $docsdir..."
+mv $tempdir/docs $docsdir
+echo " Done."
+
+echo -n "Moving generated JDiff to $diffsdir..."
+mv $tempdir/diffs $diffsdir
+echo " Done."
+
+# Commit
+echo -n "Committing changes..."
+git add .
+git commit -q -m "Generate Javadoc and JDiff for Guava $mavenversion"
+echo " Done."
+
+# Update version info in _config.yml
+if [[ $release == "snapshot" ]]; then
+  fieldtoupdate="latest_snapshot"
+  version="$mavenversion"
+else
+  fieldtoupdate="latest_release"
+  # The release being updated currently may not be the latest release.
+  version=$(latest_release)
+fi
+sed -i 's/$fieldtoupdate:[ ]+.+/$fieldtoupdate: $version/g' _config.yml
+if ! git diff --quiet ; then
+  echo -n "Updating $fieldtoupdate in _config.yml to $version..."
+  git add _config.yml > /dev/null
+  git commit -q -m "Update $fieldtoupdate version to $version"
+  echo " Done."
+fi
+
+echo "Update succeeded."
