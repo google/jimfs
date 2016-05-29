9bb8f7bd5a92ac6ee7c73ff1015eb27424c01cc1
sahmed10315 <sahmed10315@gmail.com>
2016-05-29 16:08:52 -0500

    Removed TODO comment for making heap disks configurable, as its already implemented

--- jimfs/src/main/java/com/google/common/jimfs/JimfsFileSystems.java
+++ jimfs/src/main/java/com/google/common/jimfs/JimfsFileSystems.java
@@ -88,7 +88,6 @@ final class JimfsFileSystems {
       Configuration config, PathService pathService, FileSystemState state) {
     AttributeService attributeService = new AttributeService(config);
 
-    // TODO(cgdecker): Make disk values configurable
     HeapDisk disk = new HeapDisk(config);
     FileFactory fileFactory = new FileFactory(disk);
 
