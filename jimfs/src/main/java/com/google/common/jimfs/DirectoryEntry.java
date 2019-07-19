3520262d4ae1224ab336fbf71ccd1d22d40a01f7
Alexandre M. S. Lima <alexandre.lima@comexport.com.br>
2019-07-19 14:29:15 -0300

    Add implements Serializable

--- jimfs/src/main/java/com/google/common/jimfs/DirectoryEntry.java
+++ jimfs/src/main/java/com/google/common/jimfs/DirectoryEntry.java
@@ -20,6 +20,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
 import com.google.common.base.MoreObjects;
+
+import java.io.Serializable;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.NotDirectoryException;
@@ -29,139 +31,146 @@ import java.util.Objects;
 import javax.annotation.Nullable;
 
 /**
- * Entry in a directory, containing references to the directory itself, the file the entry links to
- * and the name of the entry.
+ * Entry in a directory, containing references to the directory itself, the file
+ * the entry links to and the name of the entry.
  *
- * <p>May also represent a non-existent entry if the name does not link to any file in the
- * directory.
+ * <p>
+ * May also represent a non-existent entry if the name does not link to any file
+ * in the directory.
  */
-final class DirectoryEntry {
+final class DirectoryEntry implements Serializable {
 
-  private final Directory directory;
-  private final Name name;
+    private static final long serialVersionUID = -3258824108590984645L;
+    private final Directory directory;
+    private final Name name;
 
-  @Nullable private final File file;
+    @Nullable
+    private final File file;
 
-  @Nullable DirectoryEntry next; // for use in Directory
+    @Nullable
+    DirectoryEntry next; // for use in Directory
 
-  DirectoryEntry(Directory directory, Name name, @Nullable File file) {
-    this.directory = checkNotNull(directory);
-    this.name = checkNotNull(name);
-    this.file = file;
-  }
-
-  /** Returns {@code true} if and only if this entry represents an existing file. */
-  public boolean exists() {
-    return file != null;
-  }
-
-  /**
-   * Checks that this entry exists, throwing an exception if not.
-   *
-   * @return this
-   * @throws NoSuchFileException if this entry does not exist
-   */
-  public DirectoryEntry requireExists(Path pathForException) throws NoSuchFileException {
-    if (!exists()) {
-      throw new NoSuchFileException(pathForException.toString());
+    DirectoryEntry(Directory directory, Name name, @Nullable File file) {
+        this.directory = checkNotNull(directory);
+        this.name = checkNotNull(name);
+        this.file = file;
     }
-    return this;
-  }
 
-  /**
-   * Checks that this entry does not exist, throwing an exception if it does.
-   *
-   * @return this
-   * @throws FileAlreadyExistsException if this entry does not exist
-   */
-  public DirectoryEntry requireDoesNotExist(Path pathForException)
-      throws FileAlreadyExistsException {
-    if (exists()) {
-      throw new FileAlreadyExistsException(pathForException.toString());
+    /**
+     * Returns {@code true} if and only if this entry represents an existing file.
+     */
+    public boolean exists() {
+        return file != null;
     }
-    return this;
-  }
 
-  /**
-   * Checks that this entry exists and links to a directory, throwing an exception if not.
-   *
-   * @return this
-   * @throws NoSuchFileException if this entry does not exist
-   * @throws NotDirectoryException if this entry does not link to a directory
-   */
-  public DirectoryEntry requireDirectory(Path pathForException)
-      throws NoSuchFileException, NotDirectoryException {
-    requireExists(pathForException);
-    if (!file().isDirectory()) {
-      throw new NotDirectoryException(pathForException.toString());
+    /**
+     * Checks that this entry exists, throwing an exception if not.
+     *
+     * @return this
+     * @throws NoSuchFileException if this entry does not exist
+     */
+    public DirectoryEntry requireExists(Path pathForException) throws NoSuchFileException {
+        if (!exists()) {
+            throw new NoSuchFileException(pathForException.toString());
+        }
+        return this;
     }
-    return this;
-  }
 
-  /**
-   * Checks that this entry exists and links to a symbolic link, throwing an exception if not.
-   *
-   * @return this
-   * @throws NoSuchFileException if this entry does not exist
-   * @throws NotLinkException if this entry does not link to a symbolic link
-   */
-  public DirectoryEntry requireSymbolicLink(Path pathForException)
-      throws NoSuchFileException, NotLinkException {
-    requireExists(pathForException);
-    if (!file().isSymbolicLink()) {
-      throw new NotLinkException(pathForException.toString());
+    /**
+     * Checks that this entry does not exist, throwing an exception if it does.
+     *
+     * @return this
+     * @throws FileAlreadyExistsException if this entry does not exist
+     */
+    public DirectoryEntry requireDoesNotExist(Path pathForException)
+            throws FileAlreadyExistsException {
+        if (exists()) {
+            throw new FileAlreadyExistsException(pathForException.toString());
+        }
+        return this;
     }
-    return this;
-  }
 
-  /** Returns the directory containing this entry. */
-  public Directory directory() {
-    return directory;
-  }
-
-  /** Returns the name of this entry. */
-  public Name name() {
-    return name;
-  }
-
-  /**
-   * Returns the file this entry links to.
-   *
-   * @throws IllegalStateException if the file does not exist
-   */
-  public File file() {
-    checkState(exists());
-    return file;
-  }
-
-  /** Returns the file this entry links to or {@code null} if the file does not exist */
-  @Nullable
-  public File fileOrNull() {
-    return file;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj instanceof DirectoryEntry) {
-      DirectoryEntry other = (DirectoryEntry) obj;
-      return directory.equals(other.directory)
-          && name.equals(other.name)
-          && Objects.equals(file, other.file);
+    /**
+     * Checks that this entry exists and links to a directory, throwing an exception
+     * if not.
+     *
+     * @return this
+     * @throws NoSuchFileException   if this entry does not exist
+     * @throws NotDirectoryException if this entry does not link to a directory
+     */
+    public DirectoryEntry requireDirectory(Path pathForException)
+            throws NoSuchFileException, NotDirectoryException {
+        requireExists(pathForException);
+        if (!file().isDirectory()) {
+            throw new NotDirectoryException(pathForException.toString());
+        }
+        return this;
     }
-    return false;
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(directory, name, file);
-  }
+    /**
+     * Checks that this entry exists and links to a symbolic link, throwing an
+     * exception if not.
+     *
+     * @return this
+     * @throws NoSuchFileException if this entry does not exist
+     * @throws NotLinkException    if this entry does not link to a symbolic link
+     */
+    public DirectoryEntry requireSymbolicLink(Path pathForException)
+            throws NoSuchFileException, NotLinkException {
+        requireExists(pathForException);
+        if (!file().isSymbolicLink()) {
+            throw new NotLinkException(pathForException.toString());
+        }
+        return this;
+    }
 
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(this)
-        .add("directory", directory)
-        .add("name", name)
-        .add("file", file)
-        .toString();
-  }
+    /** Returns the directory containing this entry. */
+    public Directory directory() {
+        return directory;
+    }
+
+    /** Returns the name of this entry. */
+    public Name name() {
+        return name;
+    }
+
+    /**
+     * Returns the file this entry links to.
+     *
+     * @throws IllegalStateException if the file does not exist
+     */
+    public File file() {
+        checkState(exists());
+        return file;
+    }
+
+    /**
+     * Returns the file this entry links to or {@code null} if the file does not
+     * exist
+     */
+    @Nullable
+    public File fileOrNull() {
+        return file;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof DirectoryEntry) {
+            DirectoryEntry other = (DirectoryEntry) obj;
+            return directory.equals(other.directory) && name.equals(other.name)
+                    && Objects.equals(file, other.file);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(directory, name, file);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("directory", directory).add("name", name)
+                .add("file", file).toString();
+    }
 }
--- jimfs/src/main/java/com/google/common/jimfs/File.java
+++ jimfs/src/main/java/com/google/common/jimfs/File.java
@@ -24,6 +24,7 @@ import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Table;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.concurrent.locks.ReadWriteLock;
 import javax.annotation.Nullable;
 
@@ -32,249 +33,262 @@ import javax.annotation.Nullable;
  *
  * @author Colin Decker
  */
-public abstract class File {
+public abstract class File implements Serializable {
 
-  private final int id;
+    private static final long serialVersionUID = -2251576112826413721L;
 
-  private int links;
+    private final int id;
 
-  private long creationTime;
-  private long lastAccessTime;
-  private long lastModifiedTime;
+    private int links;
 
-  @Nullable // null when only the basic view is used (default)
-  private Table<String, String, Object> attributes;
+    private long creationTime;
+    private long lastAccessTime;
+    private long lastModifiedTime;
 
-  File(int id) {
-    this.id = id;
+    @Nullable // null when only the basic view is used (default)
+    private Table<String, String, Object> attributes;
 
-    long now = System.currentTimeMillis(); // TODO(cgdecker): Use a Clock
-    this.creationTime = now;
-    this.lastAccessTime = now;
-    this.lastModifiedTime = now;
-  }
+    File(int id) {
+        this.id = id;
 
-  /** Returns the ID of this file. */
-  public int id() {
-    return id;
-  }
-
-  /**
-   * Returns the size, in bytes, of this file's content. Directories and symbolic links have a size
-   * of 0.
-   */
-  public long size() {
-    return 0;
-  }
-
-  /** Returns whether or not this file is a directory. */
-  public final boolean isDirectory() {
-    return this instanceof Directory;
-  }
-
-  /** Returns whether or not this file is a regular file. */
-  public final boolean isRegularFile() {
-    return this instanceof RegularFile;
-  }
-
-  /** Returns whether or not this file is a symbolic link. */
-  public final boolean isSymbolicLink() {
-    return this instanceof SymbolicLink;
-  }
-
-  /**
-   * Creates a new file of the same type as this file with the given ID. Does not copy the content
-   * of this file unless the cost of copying the content is minimal. This is because this method is
-   * called with a hold on the file system's lock.
-   */
-  abstract File copyWithoutContent(int id);
-
-  /**
-   * Copies the content of this file to the given file. The given file must be the same type of file
-   * as this file and should have no content.
-   *
-   * <p>This method is used for copying the content of a file after copying the file itself. Does
-   * nothing by default.
-   */
-  void copyContentTo(File file) throws IOException {}
-
-  /**
-   * Returns the read-write lock for this file's content, or {@code null} if there is no content
-   * lock.
-   */
-  @Nullable
-  ReadWriteLock contentLock() {
-    return null;
-  }
-
-  /** Called when a stream or channel to this file is opened. */
-  void opened() {}
-
-  /**
-   * Called when a stream or channel to this file is closed. If there are no more streams or
-   * channels open to the file and it has been deleted, its contents may be deleted.
-   */
-  void closed() {}
-
-  /**
-   * Called when (a single link to) this file is deleted. There may be links remaining. Does nothing
-   * by default.
-   */
-  void deleted() {}
-
-  /** Returns whether or not this file is a root directory of the file system. */
-  final boolean isRootDirectory() {
-    // only root directories have their parent link pointing to themselves
-    return isDirectory() && equals(((Directory) this).parent());
-  }
-
-  /** Returns the current count of links to this file. */
-  public final synchronized int links() {
-    return links;
-  }
-
-  /**
-   * Called when this file has been linked in a directory. The given entry is the new directory
-   * entry that links to this file.
-   */
-  void linked(DirectoryEntry entry) {
-    checkNotNull(entry);
-  }
-
-  /** Called when this file has been unlinked from a directory, either for a move or delete. */
-  void unlinked() {}
-
-  /** Increments the link count for this file. */
-  final synchronized void incrementLinkCount() {
-    links++;
-  }
-
-  /** Decrements the link count for this file. */
-  final synchronized void decrementLinkCount() {
-    links--;
-  }
-
-  /** Gets the creation time of the file. */
-  @SuppressWarnings("GoodTime") // should return a java.time.Instant
-  public final synchronized long getCreationTime() {
-    return creationTime;
-  }
-
-  /** Gets the last access time of the file. */
-  @SuppressWarnings("GoodTime") // should return a java.time.Instant
-  public final synchronized long getLastAccessTime() {
-    return lastAccessTime;
-  }
-
-  /** Gets the last modified time of the file. */
-  @SuppressWarnings("GoodTime") // should return a java.time.Instant
-  public final synchronized long getLastModifiedTime() {
-    return lastModifiedTime;
-  }
-
-  /** Sets the creation time of the file. */
-  final synchronized void setCreationTime(long creationTime) {
-    this.creationTime = creationTime;
-  }
-
-  /** Sets the last access time of the file. */
-  final synchronized void setLastAccessTime(long lastAccessTime) {
-    this.lastAccessTime = lastAccessTime;
-  }
-
-  /** Sets the last modified time of the file. */
-  final synchronized void setLastModifiedTime(long lastModifiedTime) {
-    this.lastModifiedTime = lastModifiedTime;
-  }
-
-  /** Sets the last access time of the file to the current time. */
-  final void updateAccessTime() {
-    setLastAccessTime(System.currentTimeMillis());
-  }
-
-  /** Sets the last modified time of the file to the current time. */
-  final void updateModifiedTime() {
-    setLastModifiedTime(System.currentTimeMillis());
-  }
-
-  /**
-   * Returns the names of the attributes contained in the given attribute view in the file's
-   * attributes table.
-   */
-  public final synchronized ImmutableSet<String> getAttributeNames(String view) {
-    if (attributes == null) {
-      return ImmutableSet.of();
-    }
-    return ImmutableSet.copyOf(attributes.row(view).keySet());
-  }
-
-  /** Returns the attribute keys contained in the attributes map for the file. */
-  @VisibleForTesting
-  final synchronized ImmutableSet<String> getAttributeKeys() {
-    if (attributes == null) {
-      return ImmutableSet.of();
+        long now = System.currentTimeMillis(); // TODO(cgdecker): Use a Clock
+        this.creationTime = now;
+        this.lastAccessTime = now;
+        this.lastModifiedTime = now;
     }
 
-    ImmutableSet.Builder<String> builder = ImmutableSet.builder();
-    for (Table.Cell<String, String, Object> cell : attributes.cellSet()) {
-      builder.add(cell.getRowKey() + ':' + cell.getColumnKey());
+    /** Returns the ID of this file. */
+    public int id() {
+        return id;
     }
-    return builder.build();
-  }
 
-  /** Gets the value of the given attribute in the given view. */
-  @Nullable
-  public final synchronized Object getAttribute(String view, String attribute) {
-    if (attributes == null) {
-      return null;
+    /**
+     * Returns the size, in bytes, of this file's content. Directories and symbolic
+     * links have a size of 0.
+     */
+    public long size() {
+        return 0;
     }
-    return attributes.get(view, attribute);
-  }
 
-  /** Sets the given attribute in the given view to the given value. */
-  public final synchronized void setAttribute(String view, String attribute, Object value) {
-    if (attributes == null) {
-      attributes = HashBasedTable.create();
+    /** Returns whether or not this file is a directory. */
+    public final boolean isDirectory() {
+        return this instanceof Directory;
     }
-    attributes.put(view, attribute, value);
-  }
 
-  /** Deletes the given attribute from the given view. */
-  public final synchronized void deleteAttribute(String view, String attribute) {
-    if (attributes != null) {
-      attributes.remove(view, attribute);
+    /** Returns whether or not this file is a regular file. */
+    public final boolean isRegularFile() {
+        return this instanceof RegularFile;
     }
-  }
 
-  /** Copies basic attributes (file times) from this file to the given file. */
-  final synchronized void copyBasicAttributes(File target) {
-    target.setFileTimes(creationTime, lastModifiedTime, lastAccessTime);
-  }
-
-  private synchronized void setFileTimes(
-      long creationTime, long lastModifiedTime, long lastAccessTime) {
-    this.creationTime = creationTime;
-    this.lastModifiedTime = lastModifiedTime;
-    this.lastAccessTime = lastAccessTime;
-  }
-
-  /** Copies the attributes from this file to the given file. */
-  final synchronized void copyAttributes(File target) {
-    copyBasicAttributes(target);
-    target.putAll(attributes);
-  }
-
-  private synchronized void putAll(@Nullable Table<String, String, Object> attributes) {
-    if (attributes != null && this.attributes != attributes) {
-      if (this.attributes == null) {
-        this.attributes = HashBasedTable.create();
-      }
-      this.attributes.putAll(attributes);
+    /** Returns whether or not this file is a symbolic link. */
+    public final boolean isSymbolicLink() {
+        return this instanceof SymbolicLink;
     }
-  }
 
-  @Override
-  public final String toString() {
-    return MoreObjects.toStringHelper(this).add("id", id()).toString();
-  }
+    /**
+     * Creates a new file of the same type as this file with the given ID. Does not
+     * copy the content of this file unless the cost of copying the content is
+     * minimal. This is because this method is called with a hold on the file
+     * system's lock.
+     */
+    abstract File copyWithoutContent(int id);
+
+    /**
+     * Copies the content of this file to the given file. The given file must be the
+     * same type of file as this file and should have no content.
+     *
+     * <p>
+     * This method is used for copying the content of a file after copying the file
+     * itself. Does nothing by default.
+     */
+    void copyContentTo(File file) throws IOException {
+    }
+
+    /**
+     * Returns the read-write lock for this file's content, or {@code null} if there
+     * is no content lock.
+     */
+    @Nullable
+    ReadWriteLock contentLock() {
+        return null;
+    }
+
+    /** Called when a stream or channel to this file is opened. */
+    void opened() {
+    }
+
+    /**
+     * Called when a stream or channel to this file is closed. If there are no more
+     * streams or channels open to the file and it has been deleted, its contents
+     * may be deleted.
+     */
+    void closed() {
+    }
+
+    /**
+     * Called when (a single link to) this file is deleted. There may be links
+     * remaining. Does nothing by default.
+     */
+    void deleted() {
+    }
+
+    /** Returns whether or not this file is a root directory of the file system. */
+    final boolean isRootDirectory() {
+        // only root directories have their parent link pointing to themselves
+        return isDirectory() && equals(((Directory) this).parent());
+    }
+
+    /** Returns the current count of links to this file. */
+    public final synchronized int links() {
+        return links;
+    }
+
+    /**
+     * Called when this file has been linked in a directory. The given entry is the
+     * new directory entry that links to this file.
+     */
+    void linked(DirectoryEntry entry) {
+        checkNotNull(entry);
+    }
+
+    /**
+     * Called when this file has been unlinked from a directory, either for a move
+     * or delete.
+     */
+    void unlinked() {
+    }
+
+    /** Increments the link count for this file. */
+    final synchronized void incrementLinkCount() {
+        links++;
+    }
+
+    /** Decrements the link count for this file. */
+    final synchronized void decrementLinkCount() {
+        links--;
+    }
+
+    /** Gets the creation time of the file. */
+    @SuppressWarnings("GoodTime") // should return a java.time.Instant
+    public final synchronized long getCreationTime() {
+        return creationTime;
+    }
+
+    /** Gets the last access time of the file. */
+    @SuppressWarnings("GoodTime") // should return a java.time.Instant
+    public final synchronized long getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    /** Gets the last modified time of the file. */
+    @SuppressWarnings("GoodTime") // should return a java.time.Instant
+    public final synchronized long getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    /** Sets the creation time of the file. */
+    final synchronized void setCreationTime(long creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    /** Sets the last access time of the file. */
+    final synchronized void setLastAccessTime(long lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+
+    /** Sets the last modified time of the file. */
+    final synchronized void setLastModifiedTime(long lastModifiedTime) {
+        this.lastModifiedTime = lastModifiedTime;
+    }
+
+    /** Sets the last access time of the file to the current time. */
+    final void updateAccessTime() {
+        setLastAccessTime(System.currentTimeMillis());
+    }
+
+    /** Sets the last modified time of the file to the current time. */
+    final void updateModifiedTime() {
+        setLastModifiedTime(System.currentTimeMillis());
+    }
+
+    /**
+     * Returns the names of the attributes contained in the given attribute view in
+     * the file's attributes table.
+     */
+    public final synchronized ImmutableSet<String> getAttributeNames(String view) {
+        if (attributes == null) {
+            return ImmutableSet.of();
+        }
+        return ImmutableSet.copyOf(attributes.row(view).keySet());
+    }
+
+    /** Returns the attribute keys contained in the attributes map for the file. */
+    @VisibleForTesting
+    final synchronized ImmutableSet<String> getAttributeKeys() {
+        if (attributes == null) {
+            return ImmutableSet.of();
+        }
+
+        ImmutableSet.Builder<String> builder = ImmutableSet.builder();
+        for (Table.Cell<String, String, Object> cell : attributes.cellSet()) {
+            builder.add(cell.getRowKey() + ':' + cell.getColumnKey());
+        }
+        return builder.build();
+    }
+
+    /** Gets the value of the given attribute in the given view. */
+    @Nullable
+    public final synchronized Object getAttribute(String view, String attribute) {
+        if (attributes == null) {
+            return null;
+        }
+        return attributes.get(view, attribute);
+    }
+
+    /** Sets the given attribute in the given view to the given value. */
+    public final synchronized void setAttribute(String view, String attribute, Object value) {
+        if (attributes == null) {
+            attributes = HashBasedTable.create();
+        }
+        attributes.put(view, attribute, value);
+    }
+
+    /** Deletes the given attribute from the given view. */
+    public final synchronized void deleteAttribute(String view, String attribute) {
+        if (attributes != null) {
+            attributes.remove(view, attribute);
+        }
+    }
+
+    /** Copies basic attributes (file times) from this file to the given file. */
+    final synchronized void copyBasicAttributes(File target) {
+        target.setFileTimes(creationTime, lastModifiedTime, lastAccessTime);
+    }
+
+    private synchronized void setFileTimes(long creationTime, long lastModifiedTime,
+            long lastAccessTime) {
+        this.creationTime = creationTime;
+        this.lastModifiedTime = lastModifiedTime;
+        this.lastAccessTime = lastAccessTime;
+    }
+
+    /** Copies the attributes from this file to the given file. */
+    final synchronized void copyAttributes(File target) {
+        copyBasicAttributes(target);
+        target.putAll(attributes);
+    }
+
+    private synchronized void putAll(@Nullable Table<String, String, Object> attributes) {
+        if (attributes != null && this.attributes != attributes) {
+            if (this.attributes == null) {
+                this.attributes = HashBasedTable.create();
+            }
+            this.attributes.putAll(attributes);
+        }
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("id", id()).toString();
+    }
 }
